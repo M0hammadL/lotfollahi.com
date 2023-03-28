e4c08761e098ab70f0464816412debce2bb1a8c1
ronskoro <rskorobogat@gmail.com>
2023-03-28 16:36:25 +0200

    edited the landing page

--- src/info/Info.js
+++ src/info/Info.js
@@ -2,10 +2,10 @@ let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
 colors = ["white", "black"]
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    firstName: "Mohammad",
+    lastName: "Lotfollahi",
+    initials: "ML", // the example uses first and last, but feel free to use three or more if you like.
+    position: "Postdoctoral Researcher",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,30 +3,8 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Landing from '@site/src/components/Landing';
 import About from '@site/src/components/About';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -34,11 +12,9 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
       <main>
         <Landing />
         <About />
-        <HomepageFeatures />
       </main>
     </Layout>
   );
